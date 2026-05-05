8131b006a624cc2395921b538397dfc9f660277c
ShubHH03 <mak.shubham2003@gmail.com>
2026-05-06 00:26:11 +0530

    implement video fetching and display in App component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,46 @@
-
+import { useEffect, useState } from 'react';
 
 function App() {
 
-  return (
-    <>
-    <h1>Video Hub</h1>
-    </>
-  )
+  const [videos, setVideos] = useState([]);
+
+	useEffect(() => {
+		const fetchVideos = async () => {
+			try {
+
+        const response = await fetch('https://api.freeapi.app/api/v1/public/youtube/videos')
+        const data = await response.json();
+        console.log('Fetched videos:', data.data.data[0].items.snippet);
+        const videoData = data.data.data
+        setVideos(videoData);
+
+			} catch (error) {
+				console.log('error fetching videos:', error);
+			}
+		};
+		fetchVideos();
+	}, []);
+	return (
+		<>
+			<h1>Video Hub</h1>
+      <div>
+        {videos.map((video) => (
+          <div key={video.items.id} >
+            <h2>{video.items.snippet.title}</h2>
+            <iframe 
+              width="560" 
+              height="315" 
+              src={`https://www.youtube.com/embed/${video.items.id}`} 
+              title={video.items.snippet.title} 
+              frameBorder="0" 
+              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+              allowFullScreen
+            ></iframe>
+          </div>
+        ))}
+      </div>
+		</>
+	);
 }
 
-export default App
+export default App;
